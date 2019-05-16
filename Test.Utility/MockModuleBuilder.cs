7840e110c2b0e6f0a6a4b94efb404dc88973f662
Dominic Groth <dominic.groth@messerli.ch>
2019-05-16 08:17:03 +0200

    Add mock module builder

---
+++ Test.Utility/MockModuleBuilder.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Core;
+using Moq;
+
+namespace Messerli.Test.Utility
+{
+    public class MockModuleBuilder
+    {
+        private readonly List<MockModule.Register> _mockRegistrations = new List<MockModule.Register>();
+
+        public MockModuleBuilder RegisterMockFor<TInterface>() where TInterface : class
+        {
+            _mockRegistrations.Add(
+                builder => builder.Register(context => new Mock<TInterface>().Object).As<TInterface>());
+
+            return this;
+        }
+
+        public MockModuleBuilder MockEnvironmentVariable(string variable, string value)
+        {
+            Environment.SetEnvironmentVariable(variable, value);
+
+            return this;
+        }
+
+        public IModule Build()
+        {
+            return new MockModule(_mockRegistrations);
+        }
+    }
+}
