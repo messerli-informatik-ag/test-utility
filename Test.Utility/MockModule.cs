7e9fcea6adb7ca5495cac669e1e782e4b88a3e62
Dominic Groth <dominic.groth@messerli.ch>
2019-05-16 08:16:39 +0200

    Add mock module

---
+++ Test.Utility/MockModule.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Autofac;
+
+namespace Messerli.Test.Utility
+{
+    public class MockModule : Module
+    {
+        public delegate void Register(ContainerBuilder builder);
+
+        public MockModule(List<Register> mockRegistrations)
+        {
+            _mockRegistrations = mockRegistrations;
+        }
+
+        private readonly List<Register> _mockRegistrations;
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            foreach (var registration in _mockRegistrations)
+            {
+                registration(builder);
+            }
+        }
+    }
+}
