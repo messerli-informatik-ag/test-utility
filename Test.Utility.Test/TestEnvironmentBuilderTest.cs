d1e0d44dd2acb07673a83aa83c5e1cf610c333e5
Sergio Sutter <sergio.sutter@messerli.ch>
2019-09-25 11:07:45 +0200

    created TestEnvironmentBuilder

---
+++ Test.Utility.Test/TestEnvironmentBuilderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Xunit;
+
+namespace Messerli.Test.Utility.Test
+{
+    public class TestEnvironmentBuilderTest 
+    {
+        [Fact]
+        public void CreateTestEnvironmentBuilder()
+        {
+            var testEnvironmentBuilder = new TestEnvironmentBuilder();
+            
+            var testFile = new TestFile("file1.txt");
+            testEnvironmentBuilder.AddTestFile(testFile);
+            var testEnvironmentProvider = testEnvironmentBuilder.Build();
+
+            var tempPath = Path.GetTempPath();
+            Assert.True(File.Exists(Path.Combine(tempPath, testEnvironmentProvider.RootDirectory, testFile.RelativeFilePath)));
+
+        }
+    }
+}
---
+++ Test.Utility/TestEnvironmentBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Castle.Core;
+using Messerli.Test.Utility;
+
+namespace Messerli.Test.Utility
+{
+    public class TestEnvironmentBuilder
+    {
+        private readonly List<TestFile> _testFiles = new List<TestFile>();
+
+        public TestEnvironmentBuilder AddTestFile(TestFile file)
+        {
+            _testFiles.Add(file);
+            return this;
+        }
+
+        public TestEnvironmentProvider Build()
+        {
+            return new TestEnvironmentProvider(_testFiles);
+        }
+    }
+}
