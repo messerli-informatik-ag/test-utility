95cc89bfe596682294190b352fc9ef06cc70bdf3
Sergio Sutter <sergio.sutter@messerli.ch>
2019-10-03 09:49:07 +0200

    Add UnitTest

---
+++ Test.Utility.Test/TestFileTest.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Net;
+using Xunit;
+
+namespace Messerli.Test.Utility.Test
+{
+    public class TestFileTest
+    {
+        [Fact]
+        public void Create()
+        {
+            var testFile = TestFile.Create(typeof(TestFileTest).Assembly, "");
+            Assert.True(File.Exists(testFile.SourceFilePath));
+        }
+    }
+}
