60c48fcac562077f31faaf661e2e8716b8986b39
Sergio Sutter <sergio.sutter@messerli.ch>
2020-01-08 11:18:55 +0100

    Add ContainerInterfaceRetriever

---
+++ Test.Utility/ContainerInterfaceRetriever.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Autofac;
+
+namespace Messerli.Test.Utility
+{
+    public static class ContainerInterfaceRetriever
+    {
+        public static IEnumerable<Type> GetAssemblyInterfaces(IContainer container)
+            => container
+                .ComponentRegistry
+                .Registrations
+                .Select(registration => registration.Activator.LimitType.Assembly)
+                .SelectMany(GetAssemblyTypes)
+                .ToHashSet();
+
+        private static IEnumerable<Type> GetAssemblyTypes(Assembly assembly)
+            => assembly
+                .GetTypes()
+                .Where(type => type.IsInterface && !NamespaceContains(type, nameof(Autofac)));
+
+        private static bool NamespaceContains(Type type, string @string)
+            => type.Namespace?.Contains(@string) ?? false;
+    }
+}
