0cca66b70da6f5b9ad63d3825d71119886db021e
Sergio Sutter <sergio.sutter@messerli.ch>
2020-01-10 11:35:48 +0100

    Add mock builders

--- Test.Utility/MockModuleBuilder.cs
+++ Test.Utility/MockModuleBuilder.cs
@@ -1,51 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using Autofac;
-using Autofac.Core;
+﻿using Messerli.CompositionRoot;
 using Moq;
 
 namespace Messerli.Test.Utility
 {
-    public class MockModuleBuilder
+    public static class MockModuleBuilder
     {
-        private readonly List<MockModule.Register> _mockRegistrations = new List<MockModule.Register>();
+        public static void RegisterMock<T>(this ModuleBuilder moduleBuilder)
+            where T : class
+            => moduleBuilder.Register(builder => builder.RegisterMock<T>());
 
-        public MockModuleBuilder RegisterMockFor<TInterface>()
-            where TInterface : class
-            => Register(builder => builder.Register(context => new Mock<TInterface>().Object).As<TInterface>());
-
-        public MockModuleBuilder RegisterMockFor<TInterface>(Mock<TInterface> mock)
-            where TInterface : class
-            => Register(builder => builder.Register(context => mock.Object).As<TInterface>());
-
-        public MockModuleBuilder RegisterModule<TModule>()
-            where TModule : IModule, new()
-            => Register(builder => builder.RegisterModule<TModule>());
-
-        public MockModuleBuilder RegisterModule<TModule>(TModule module)
-            where TModule : IModule
-            => Register(builder => builder.RegisterModule(module));
-
-        public MockModuleBuilder RegisterInstance<TInstance>(TInstance instance)
-            where TInstance : class
-            => Register(builder => builder.RegisterInstance(instance));
-
-        public MockModuleBuilder Register(MockModule.Register registrationFunction)
-        {
-            _mockRegistrations.Add(registrationFunction);
-            return this;
-        }
-
-        public MockModuleBuilder MockEnvironmentVariable(string variable, string value)
-        {
-            Environment.SetEnvironmentVariable(variable, value);
-
-            return this;
-        }
-
-        public IModule Build()
-        {
-            return new MockModule(_mockRegistrations);
-        }
+        public static void RegisterMock<T>(this ModuleBuilder moduleBuilder, Mock<T> mock)
+            where T : class
+            => moduleBuilder.Register(builder => builder.RegisterMock(mock));
     }
 }
