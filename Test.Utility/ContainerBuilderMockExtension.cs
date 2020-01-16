f2a5e6cbb734f53b81fa3bdf488b93e719419053
Sergio Sutter <sergio.sutter@messerli.ch>
2020-01-16 13:17:48 +0100

    Adapt return type of mock extension methods

--- Test.Utility/ContainerBuilderMockExtension.cs
+++ Test.Utility/ContainerBuilderMockExtension.cs
@@ -1,15 +1,16 @@
 ﻿using Autofac;
+using Autofac.Builder;
 using Moq;
 
 namespace Messerli.Test.Utility
 {
     public static class ContainerBuilderMockExtension
     {
-        public static void RegisterMock<T>(this ContainerBuilder builder)
+        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterMock<T>(this ContainerBuilder builder)
             where T : class
             => builder.Register(context => new Mock<T>().Object).As<T>();
 
-        public static void RegisterMock<T>(this ContainerBuilder builder, Mock<T> mock)
+        public static IRegistrationBuilder<T, SimpleActivatorData, SingleRegistrationStyle> RegisterMock<T>(this ContainerBuilder builder, Mock<T> mock)
             where T : class
             => builder.Register(context => mock.Object).As<T>();
     }
