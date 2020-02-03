c6b403ef879487f7e0e1f888a8a677657f6039fb
Dominic Groth <dominic.groth@messerli.ch>
2020-02-03 16:13:09 +0100

    Add tests

---
+++ Test.Utility.Test/ContainerInterfaceRetrieverTest.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac.Core;
+using Messerli.CompositionRoot;
+using Messerli.Test.Utility.Test.Stubs;
+using Xunit;
+
+namespace Messerli.Test.Utility.Test
+{
+    public class ContainerInterfaceRetrieverTest
+    {
+        [Theory]
+        [MemberData(nameof(GetTestModules))]
+        public void ReturnsExpectedInterfaces(IEnumerable<Type> expected, IModule module)
+        {
+            var container = new CompositionRootBuilder()
+                .RegisterModule(module)
+                .Build();
+            
+            Assert.Equal(expected, ContainerInterfaceRetriever.GetAssemblyInterfaces(container).ToArray());
+        }
+
+        public static TheoryData<IEnumerable<Type>, IModule> GetTestModules()
+            => new TheoryData<IEnumerable<Type>, IModule>
+            {
+                { new[] { typeof(ITestInterface), typeof(ITestInterface2) }, new ModuleBuilder().RegisterMock<ITestInterface>().Build() },
+                { new Type[0], new ModuleBuilder().RegisterMock<IEnumerable<string>>().Build() },
+                { new Type[0], new ModuleBuilder().RegisterMock<IModule>().Build() },
+            };
+    }
+}
---
+++ Test.Utility.Test/Stubs/ITestInterface.cs
@@ -0,0 +1,10 @@
+namespace Messerli.Test.Utility.Test.Stubs
+{
+    public interface ITestInterface
+    {
+    }
+
+    public interface ITestInterface2
+    {
+    }
+}
