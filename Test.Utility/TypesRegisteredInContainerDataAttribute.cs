91525737cb7c41454c6ab4fba3dbd0a087f79e53
Ruben Schmidmeister <ruben.schmidmeister@messerli.ch>
2020-04-06 14:01:03 +0200

    Add attribute for retrieving types from a container as theory data

---
+++ Test.Utility/TypesRegisteredInContainerDataAttribute.cs
@@ -0,0 +1,70 @@
+﻿#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+using System.Threading.Tasks;
+using Autofac;
+using Xunit.Sdk;
+using static Messerli.Test.Utility.TypesRegisteredInContainerRetriever;
+
+namespace Messerli.Test.Utility
+{
+    /// <summary>
+    /// <para>
+    ///     Provides all types registered in an Autofac <see cref="IContainer"/> as theory data.
+    /// </para>
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// using System;
+    /// using Autofac;
+    /// using Messerli.CompositionRoot;
+    /// using Xunit;
+    ///
+    /// public sealed class FooModuleTest : IDisposable
+    /// {
+    ///     private readonly IContainer _container = CreateContainer();
+    ///
+    ///     public static IContainer CreateContainer() => new CompositionRootBuilder().Build();
+    ///
+    ///     [Theory]
+    ///     [TypesRegisteredInContainerData(nameof(CreateContainer))]
+    ///     public void TypesRegisteredInContainerCanBeResolved(Type type) => _container.Resolve(type);
+    ///
+    ///     public void Dispose() => _container.Dispose();
+    /// }
+    /// </code>
+    /// </example>
+    public sealed class TypesRegisteredInContainerDataAttribute : DataAttribute
+    {
+        private readonly string _createContainerMethodName;
+
+        public TypesRegisteredInContainerDataAttribute(string createContainerMethodName)
+        {
+            _createContainerMethodName = createContainerMethodName;
+        }
+
+        public override IEnumerable<object[]> GetData(MethodInfo testMethod)
+        {
+            try
+            {
+                return GetTypesRegisteredInContainer(testMethod).Result;
+            }
+            catch (AggregateException exception) when (exception.Flatten().InnerExceptions.Count == 1)
+            {
+                ExceptionDispatchInfo.Capture(exception.Flatten().InnerException).Throw();
+                throw null!;
+            }
+        }
+
+        private async Task<IEnumerable<object[]>> GetTypesRegisteredInContainer(MemberInfo testMethod)
+        {
+            var targetType = testMethod.DeclaringType ?? throw new NullReferenceException();
+            var types = await GetTypesRegisteredInContainerViaMethod(targetType, _createContainerMethodName);
+            return types.Select(type => new[] { type });
+        }
+    }
+}
---
+++ Test.Utility/TypesRegisteredInContainerRetriever.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace Messerli.Test.Utility
+{
+    internal static class TypesRegisteredInContainerRetriever
+    {
+        public static async Task<IEnumerable<Type>> GetTypesRegisteredInContainerViaMethod(IReflect targetType, string createContainerMethodName)
+        {
+            await using var container = CreateContainer(targetType, createContainerMethodName);
+            return container.GetRegisteredTypes();
+        }
+
+        private static IContainer CreateContainer(IReflect targetType, string createContainerMethodName)
+        {
+            var createContainerMethod = GetCreateContainerMethod(targetType, createContainerMethodName);
+            return (IContainer?)createContainerMethod.Invoke(null, null)
+                   ?? throw new NullReferenceException();
+        }
+
+        private static MethodInfo GetCreateContainerMethod(IReflect type, string createContainerMethodName)
+        {
+            var createContainerMethod = type.GetMethod(createContainerMethodName, BindingFlags.Static | BindingFlags.Public)
+                ?? throw new InvalidOperationException("Unable to access container creation method. Make sure it's static and public.");
+            ValidateCreateContainerParameters(createContainerMethod);
+            ValidateCreateContainerReturnType(createContainerMethod);
+            return createContainerMethod;
+        }
+
+        private static void ValidateCreateContainerParameters(MethodInfo method)
+        {
+            if (method.GetParameters().Any())
+            {
+                throw new InvalidOperationException("The container creation method must be nullary.");
+            }
+        }
+
+        private static void ValidateCreateContainerReturnType(MethodInfo method)
+        {
+            var expectedReturnType = typeof(IContainer);
+            var returnType = method.ReturnType;
+            if (!expectedReturnType.IsAssignableFrom(returnType))
+            {
+                throw new InvalidOperationException(
+                    $"The return type of the container creation method should be '{expectedReturnType.Name}' but was '{returnType}'");
+            }
+        }
+    }
+}
